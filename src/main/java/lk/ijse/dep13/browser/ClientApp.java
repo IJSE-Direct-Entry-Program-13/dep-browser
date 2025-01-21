6f8be6d134b2f82e6da35df73d1325ba51bd7962
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-01-21 15:30:54 +0530

    Add client app

--- src/main/java/lk/ijse/dep13/browser/ClientApp.java
+++ src/main/java/lk/ijse/dep13/browser/ClientApp.java
@@ -2,7 +2,6 @@ package lk.ijse.dep13.browser;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.Arrays;
 
 public class ClientApp {
 
@@ -10,16 +9,16 @@ public class ClientApp {
         Socket socket = new Socket("google.lk", 80);
         System.out.println("Connected to: " + socket.getRemoteSocketAddress());
 
-        new Thread(()->{
+        new Thread(() -> {
             try {
                 InputStream is = socket.getInputStream();
                 BufferedInputStream bis = new BufferedInputStream(is);
 
-                while(true){
+                while (true) {
                     byte[] buffer = new byte[1024];
                     int read = bis.read(buffer);
                     if (read == -1) break;
-                    System.out.println(new String(buffer,0,read));
+                    System.out.print(new String(buffer, 0, read));
                 }
                 System.out.println("Server stopped responding");
             } catch (IOException e) {
@@ -29,7 +28,14 @@ public class ClientApp {
 
         OutputStream os = socket.getOutputStream();
         BufferedOutputStream bos = new BufferedOutputStream(os);
-        bos.write("Hello Machan Google \n\n".getBytes());
+        String httpRequest = """
+                GET / HTTP/1.1
+                Host: google.lk
+                User-Agent: dep-browser/1.0.0
+                Connection: close
+                
+                """;
+        bos.write(httpRequest.getBytes());
         bos.flush();
     }
 }
